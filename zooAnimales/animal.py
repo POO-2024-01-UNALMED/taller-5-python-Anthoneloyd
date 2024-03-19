a4958d3d26c07c22d780f32b860409b098abb617
Santiago <agarcesb@unal.edu.co>
2024-03-19 09:35:50 -0500

    cambios

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -2,14 +2,14 @@ import zooAnimales
 
 class Animal:
     _totalAnimales = 0
-    _zona = None
+    _zona = ""
 
     def __init__(self, nombre, edad, habitat, genero):
         self.nombre = nombre
         self.edad = edad
         self.habitat = habitat
         self.genero = genero
-        self._totalAnimales += 1
+        Animal._totalAnimales += 1
 
     @staticmethod
     def movimiento():
@@ -17,14 +17,11 @@ class Animal:
 
     @classmethod
     def totalPorTipo(cls):
-        return ("Mamiferos : " + str(zooAnimales.mamifero.Mamifero.cantidadMamiferos()) +
-                "\nAves : " + str(zooAnimales.ave.Ave.cantidadAves()) +
-                "\nReptiles : " + str(zooAnimales.reptil.Reptil.cantidadReptiles()) +
-                "\nPeces : " + str(zooAnimales.pez.Pez.cantidadPeces()) +
-                "\nAnfibios : " + str(zooAnimales.anfibio.Anfibio.cantidadAnfibios()))
+        return "Mamiferos : " + str(zooAnimales.mamifero.Mamifero.cantidadMamiferos()) + "\nAves : " + str(zooAnimales.ave.Ave.cantidadAves()) + "\nReptiles : " + str(zooAnimales.reptil.Reptil.cantidadReptiles()) + "\nPeces : " + str(zooAnimales.pez.Pez.cantidadPeces()) + "\nAnfibios : " + str(zooAnimales.anfibio.Anfibio.cantidadAnfibios())
+
 
     def toString(self):
-        if self._zona == None:
+        if self._zona == "":
             return "Mi nombre es " + self._nombre + ", tengo una edad de " + str(self._edad) + ", habito en " + self._habitat + " y mi genero es " + self._genero
         else:
             return "Mi nombre es " + self._nombre + ", tengo una edad de " + str(self._edad) + ", habito en " + self._habitat + " y mi genero es " + self._genero + ", la zona en la que me ubico es " + self._zona.getNombre() + ", en el zoo " + self._zona.getZoo().getNombre()
